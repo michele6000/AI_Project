16015f407fee3cabf18eaf333938561b9f133fe7
Paola Caso <paola.caso96@gmail.com>
2020-07-21 18:03:08 +0200

    Solution Repository

---
+++ back-end/src/main/java/it/polito/ai/project/repositories/SolutionRepository.java
@@ -0,0 +1,8 @@
+package it.polito.ai.project.repositories;
+
+import it.polito.ai.project.entities.Solution;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SolutionRepository extends JpaRepository<Solution, Long> {}
