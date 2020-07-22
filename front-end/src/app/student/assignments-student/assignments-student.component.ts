30cceba31cb13ed4fdfe52c5697243ed39f8d544
Iustin Burlacu <iustin.b@axieme.com>
2020-07-22 18:58:17 +0200

    Implementata vista assignements student

--- front-end/src/app/student/assignments-student/assignments-student.component.ts
+++ front-end/src/app/student/assignments-student/assignments-student.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AssignmentsStudentComponent implements OnInit {
 
+  file: any;
+  consegne = [
+    {
+      name: 'Lab 1',
+      date: '06/07/2020',
+      history: [
+        {
+          status: 'Rivisto',
+          timestamp: '06/07/2020 14:57:12'
+        },
+        {
+          status: 'Consegnato',
+          timestamp: '06/07/2020 14:37:12'
+        }
+      ]
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  handleFileSelect($event: any) {
+    console.log($event);
+    this.file = $event.target.files[0];
+  }
 }
