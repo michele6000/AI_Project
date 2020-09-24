80be0b9bd0376342077c56ae46cf8a17c2adafab
Michele Luigi Greco <grecomichele96@gmail.com>
2020-09-24 15:38:09 +0200

    style

--- front-end/src/app/home/home.component.ts
+++ front-end/src/app/home/home.component.ts
@@ -31,114 +31,129 @@ export class HomeComponent implements OnInit {
   id="tsparticles";
 
   particlesOptions = {
-    "particles": {
-      "number": {
-        "value": 160,
-        "density": {
-          "enable": true,
-          "value_area": 800
+    fpsLimit: 120,
+    particles: {
+      number: {
+        value: 160,
+        density: {
+          enable: true,
+          value_area: 800
+        }
+        /*Speed*/
+      },
+      color: {
+        value: "#ff0000",
+        animation: {
+          enable: true,
+          speed: 2,
+          sync: true
         }
       },
-      "color": {
-        "value": "#ffffff"
-      },
-      "shape": {
-        "type": "circle",
-        "stroke": {
-          "width": 0,
-          "color": "#000000"
+      /*Dots*/
+      shape: {
+        type: "circle",
+        stroke: {
+          width: 0
         },
-        "polygon": {
-          "nb_sides": 5
+
+        polygon: {
+          nb_sides: 5
+        }
+        /*Line Speed*/
+      },
+      opacity: {
+        value: 0.5,
+        random: false,
+        anim: {
+          enable: false,
+          speed: 3,
+          opacity_min: 0.1,
+          sync: false
+        }
+      },
+      size: {
+        value: 3,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 1,
+          size_min: 0.1,
+          sync: false
+        }
+      },
+      line_linked: {
+        enable: true,
+        distance: 80,
+        color: "#ffffff",
+        opacity: 0.4,
+        width: 1
+      },
+      move: {
+        enable: true,
+        speed: 1.5,
+        direction: "none",
+        random: true,
+        straight: false,
+        out_mode: "out",
+        attract: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 1200
+        }
+      },
+      life: {
+        duration: {
+          sync: false,
+          value: 50
         },
-        "image": {
-          "src": "img/github.svg",
-          "width": 100,
-          "height": 100
-        }
-      },
-      "opacity": {
-        "value": 1,
-        "random": true,
-        "anim": {
-          "enable": true,
-          "speed": 1,
-          "opacity_min": 0,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 3,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 4,
-          "size_min": 0.3,
-          "sync": false
-        }
-      },
-      "line_linked": {
-        "enable": false,
-        "distance": 150,
-        "color": "#ffffff",
-        "opacity": 0.4,
-        "width": 1
-      },
-      "move": {
-        "enable": true,
-        "speed": 1,
-        "direction": "none",
-        "random": true,
-        "straight": false,
-        "out_mode": "out",
-        "bounce": false,
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 600
+        count: 0,
+        delay: {
+          random: {
+            enable: true,
+            minimumValue: 0.5
+          },
+          value: 1
         }
       }
     },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": true,
-          "mode": "bubble"
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: {
+          enable: true,
+          mode: "repulse"
         },
-        "onclick": {
-          "enable": true,
-          "mode": "repulse"
+        onclick: {
+          enable: true,
+          mode: "push"
         },
-        "resize": true
+        resize: true
       },
-      "modes": {
-        "grab": {
-          "distance": 400,
-          "line_linked": {
-            "opacity": 1
+      modes: {
+        grab: {
+          distance: 400,
+          line_linked: {
+            opacity: 1
           }
         },
-        "bubble": {
-          "distance": 250,
-          "size": 0,
-          "duration": 2,
-          "opacity": 0,
-          "speed": 3
+        bubble: {
+          distance: 400,
+          size: 40,
+          duration: 2,
+          opacity: 0.8
         },
-        "repulse": {
-          "distance": 400,
-          "duration": 0.4
+        repulse: {
+          distance: 50
         },
-        "push": {
-          "particles_nb": 4
+        push: {
+          particles_nb: 4
         },
-        "remove": {
-          "particles_nb": 2
+        remove: {
+          particles_nb: 2
         }
       }
     },
-    "retina_detect": true
+    retina_detect: true,
   };
 
   particlesLoaded(container): void {
