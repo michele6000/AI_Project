1167e8322eb4475faefbce5b717d5c718338876f
Michele Luigi Greco <grecomichele96@gmail.com>
2020-09-26 10:51:04 +0200

    Teams -  Beta 1

--- back-end/src/main/java/it/polito/ai/project/services/NotificationServiceImpl.java
+++ back-end/src/main/java/it/polito/ai/project/services/NotificationServiceImpl.java
@@ -46,11 +46,11 @@ public class NotificationServiceImpl implements NotificationService {
     @Override
     public void sendMessage(String address, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
-        //message.setTo(address);
+        message.setTo(address);
         message.setTo(this.address);
         message.setSubject(subject);
         message.setText(body);
-        //emailSender.send(message);
+        emailSender.send(message);
     }
 
     @Override
